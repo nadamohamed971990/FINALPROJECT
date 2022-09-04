9549706db0ba8096ad363ffb723462b79104c491
nada <nada.mohamed.ahmed9790@gmail.com>
2022-09-04 19:13:56 +0200

    First version of application moved into GitHub

--- backend/settings.py
+++ backend/settings.py
@@ -214,3 +214,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # import dj_database_url
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
+
+# Heroku: Update database configuration from $DATABASE_URL.
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
